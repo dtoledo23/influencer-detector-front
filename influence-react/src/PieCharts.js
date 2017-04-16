1c8d8d9371b003e973c310f4c0d5ba5c44aca52e
Monserrat Genereux <monserratgenereux@gmail.com>
2017-04-15 19:24:09 -0600

    Todo lo que he hecho
    No me regañes por no hacer commit antes

---
+++ influence-react/src/PieCharts.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Piecharts extends Component {
+  render() {
+    return (
+      <div className="container">
+  <div className="row">
+    <div className="col-sm-7" id="chart">
+      <h1>Chart 1</h1>
+    </div>
+    <div className="col-md-5" id="chart2">
+      <h1>Chart 2</h1>
+    </div>
+  </div>
+</div>
+      );
+  }
+}
+
+export default Piecharts;
---
+++ influence-react/src/Roster.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Roster extends React.Component {
+render() {
+return (
+<div>
+	<h1>{this.props.topInfluencer}</h1>
+	<h3>{this.props.name}</h3>
+	<h3>{this.props.likes}</h3>
+	<h3>{this.props.pageRank}</h3>
+	<h3>{this.props.ranking}</h3>
+	<a className="btn btn-outline btn-xl btn-block" id="statistics" >Comparison</a>
+</div>
+);
+}
+}
+Roster.defaultProps = {
+topInfluencer: "Top Influencer",
+name:"Name",
+likes: "Number of Likes",
+pageRank: "Page Rank",
+ranking: "Ranking"
+}
+export default Roster;
